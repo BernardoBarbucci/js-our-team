f890d10e9d5f24db44c3e2b342196828f626f8ed
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-11-28 16:43:55 +0100

    srtuttura html + array con info dei membri su js completati

--- script.js
+++ script.js
@@ -10,3 +10,37 @@
 // Trasformare la stringa foto in una immagine effettiva
 // BONUS 2:
 // Organizzare i singoli membri in card/schede, potete usare come esempio lo screenshot,  ma anche realizzare qualcosa di creativo!!!
+
+// .1 array con i membri del team
+const teamNames = [
+    {
+        name: 'Wayne Barnett',
+        position: 'Founder & CEO',
+        img: 'wayne-barnett-founder-ceo.jpg',
+    }
+    {
+        name: 'Angela Caroll',
+        position: 'Chief Editor',
+        img: 'angela-caroll-chief-editor.jpg',
+    }
+    {
+        name: 'Walter Gordon',
+        position: 'Office Manager',
+        img: 'walter-gordon-office-manager.jpg',
+    }
+    {
+        name: 'Angela Lopez',
+        position: 'Social Media Manager',
+        img: 'angela-lopez-social-media-manager.jpg',
+    }
+    {
+        name: 'Scott Estrada',
+        position: 'Developer',
+        img: 'scott-estrada-developer.jpg',
+    }
+    {
+        name: 'Barbara Ramos',
+        position: 'Graphic Designer',
+        img: 'barbara-ramos-graphic-designer.jpg',
+    }
+];
